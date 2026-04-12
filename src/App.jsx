fb9c7dd5637405c77741a86f061c66fb5261b1db
oussama <oussama.abdennadher@gmail.com>
2026-04-12 21:43:26 +0200

    chore: remove unused Supabase authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import Routes from './Routes';
-import { AuthProvider } from './contexts/AuthContext';
 import { LanguageProvider } from './contexts/LanguageContext';
 import { ApolloAppProvider } from './lib/apolloClient';
 
 function App() {
   return (
-    <AuthProvider>
-      <LanguageProvider>
-        <ApolloAppProvider>
-          <Routes />
-        </ApolloAppProvider>
-      </LanguageProvider>
-    </AuthProvider>
+    <LanguageProvider>
+      <ApolloAppProvider>
+        <Routes />
+      </ApolloAppProvider>
+    </LanguageProvider>
   );
 }
 
